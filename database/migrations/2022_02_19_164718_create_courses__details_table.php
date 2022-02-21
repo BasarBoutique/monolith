7c94f326944061ea26ec48189aeb8419512f27ae
Excelion29 <wronkowcanolt@gmail.com>
2022-02-20 22:04:22 -0500

    Install passport

--- database/migrations/2022_02_19_164718_create_courses__details_table.php
+++ database/migrations/2022_02_19_164718_create_courses__details_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('courses_details', function (Blueprint $table) {
             $table->unsignedBigInteger('course_id');
             $table->string('cdetail_author')->nullable();
-            $table->text('cdetail_description')->nullable();
+            $table->json('cdetail_description')->nullable();
             $table->timestamps();
             
             $table->foreign('course_id')->references('course_id')->on('courses')->onDelete('cascade')->onUpdate('cascade');
