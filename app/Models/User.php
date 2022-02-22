7f64fa95f3bc4e0bd81218e8a2447c0005e641c5
sastiam <sfranciach@gmail.com>
2022-02-21 23:16:12 -0500

    fix uuid generate

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -25,9 +26,6 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
-    
-    protected $primaryKey = "user_id";
     /**
      * The attributes that should be hidden for serialization.
      *
