65da39e193b029607d9ca369d5d21f46a7524bf1
sastiam <sfranciach@gmail.com>
2022-03-07 00:10:54 -0500

    fix routes api

--- routes/api/v1/permissions.php
+++ routes/api/v1/permissions.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:api', 'can:isAdmin'])->group(function () {
     Route::post('create-rol', [RolController::class, 'createRol']);
     Route::get('roles', [RolController::class, 'showRoles']);
-    Route::post('assign-rol-user', [RolUserController::class, 'assignRolToUser']);
+    Route::post('assign-rol', [RolUserController::class, 'assignRolToUser']);
 });
