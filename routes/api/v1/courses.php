dd86e942dc206ca79f1c5b882a9eff7915aaea03
sastiam <sfranciach@gmail.com>
2022-03-07 18:32:32 -0500

    Changes on course api

--- routes/api/v1/courses.php
+++ routes/api/v1/courses.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:api', 'can:isAdmin'])->group(function () {
     Route::post('create-course', [CoursesController::class, 'createCourse']);
     Route::put('update-course/{courseId}', [CoursesController::class, 'updateCourse']);
+    Route::put('change-teacher/{courseId}', [CoursesController::class, 'changeCourseTeacher']);
 });
 
