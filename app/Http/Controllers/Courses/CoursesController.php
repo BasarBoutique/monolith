51b9150f2cb03339330edbb8e324c1ea13556721
sastiam <sfranciach@gmail.com>
2022-03-07 00:12:37 -0500

    Implement createCourse endpoint

--- app/Http/Controllers/Courses/CoursesController.php
+++ app/Http/Controllers/Courses/CoursesController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Courses;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Course\StoreCourseRequest;
+use App\Services\Course\CourseService;
 use Illuminate\Http\Request;
 
 class CoursesController extends Controller
 {
-    //
+    public function createCourse(StoreCourseRequest $request)
+    {
+        $validatedRequest = $request->validated();
+
+        $service = new CourseService;
+
+        $course = $service->createCourse($validatedRequest);
+
+        return $course;
+    }
 }
