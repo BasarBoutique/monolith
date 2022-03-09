bf668c03ca3f0f438199e42aef5a4814e72dd3f6
Excelion29 <wronkowcanolt@gmail.com>
2022-03-08 21:17:48 -0500

    LOGS

--- app/Observers/Comment/CommentObserver.php
+++ app/Observers/Comment/CommentObserver.php
@@ -2,7 +2,35 @@
 
 namespace App\Observers\Comment;
 
+use App\Enums\CommentLogEnum;
+use App\Models\Comment;
+use App\Models\CommentLog;
+use Illuminate\Support\Facades\Log;
+
 class CommentObserver
 {
-    //
+    public function created(Comment $comment)
+    {
+        $commmentLog = CommentLog::create([
+            'clog_context' => CommentLogEnum::COMMENT_CREATED,
+            'clog_author' => optional(request()->user())->user_id ?? 'SYSTEM'
+        ]);
+
+        Log::info('New comment has been created', [
+            'context' => $commmentLog->clog_context,
+            'author' => $commmentLog->clog_author,
+        ]);
+    }
+
+    public function updated(Comment $comment)
+    {
+        $commmentLog = CommentLog::create([
+            'clog_context' => CommentLogEnum::COMMENT_UPDATED,
+            'clog_author' => optional(request()->user())->user_id ?? 'SYSTEM'
+        ]);
+
+        Log::info('Comment has been updated', [
+            'context' => $commmentLog->clog_context,
+        ]);
+    }
 }
