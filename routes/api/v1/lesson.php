7e5221931ae1ce57e97696863cc272b95d158e2d
sastiam <sfranciach@gmail.com>
2022-03-11 14:55:16 -0500

    cambios en la ruta

--- routes/api/v1/lesson.php
+++ routes/api/v1/lesson.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\Lesson\LessonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('lesson',[LessonController::class, 'Lesson']);
-Route::get('show-lesson/{lesson}',[LessonController::class,'showLessonById']);
+Route::get('detail/{lessonId}',[LessonController::class,'showLessonById']);
 Route::middleware(['auth:api', 'can:isAdmin'])->group(function () {
     Route::post('create-lesson', [LessonController::class,'createLesson']);
     Route::put('update-lesson/{lessonId}', [LessonController::class,'updateLesson']);
