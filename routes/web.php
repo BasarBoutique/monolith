3a3ebf0692acda4fa1ab998fea347beebb3951c2
Excelion29 <wronkowcanolt@gmail.com>
2022-03-21 17:18:35 -0500

    Vue implementacion

--- routes/web.php
+++ routes/web.php
@@ -19,46 +19,41 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/home', function () {
-    return view('dashboard');
-})->name('home');
+    Route::get('/home', function () {
+        return view('dashboard');
+    })->name('home');
 
-Route::get('profile', function(){
-    return view('profile.edit');
-})->name('profile.edit');
+    Route::get('profile', function(){
+        return view('profile.edit');
+    })->name('profile.edit');
 
-Route::get('update', function(){
-    return view('profile.edit');
-})->name('profile.update');
+    Route::get('update', function(){
+        return view('profile.edit');
+    })->name('profile.update');
 
-Route::get('password', function(){
-    return view('profile.edit');
-})->name('profile.password');
+    Route::get('password', function(){
+        return view('profile.edit');
+    })->name('profile.password');
 
-Route::get('user', function(){
-    return view('users.index');
-})->name('user.index');
+    Route::get('user', function(){
+        return view('users.index');
+    })->name('user.index');
 
-
-
-Route::get('login', function(){
-    return view('auth.login');
-})->name('login');
+    Route::get('login', function(){
+        return view('auth.login');
+    })->name('login');
 
 
-Route::get('register', function(){
-    return view('auth.register');
-})->name('register');
+    Route::get('register', function(){
+        return view('auth.register');
+    })->name('register');
 
-Route::get('logout', function(){
-    return view('welcome');
-})->name('logout');
+    Route::get('logout', function(){
+        return view('welcome');
+    })->name('logout');
 
-Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
-Route::get('map', function () {return view('pages.maps');})->name('map');
-Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
-Route::get('table-list', function () {return view('pages.tables');})->name('table');
-
-
-
+    Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
+    Route::get('map', function () {return view('pages.maps');})->name('map');
+    Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
+    Route::get('table-list', function () {return view('pages.tables');})->name('table');
 
