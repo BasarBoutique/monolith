b0f3221ed78f05d2a74bcf75c34d177bbc848c32
sastiam <sfranciach@gmail.com>
2022-04-01 19:57:30 -0500

    add fillable property

--- app/Models/CourseCategory.php
+++ app/Models/CourseCategory.php
@@ -10,16 +10,24 @@ class CourseCategory extends Pivot
 {
     use HasFactory;
 
+    const CREATED_AT = 'registered_at';
+    const UPDATED_AT = null;
+
     protected $table = "course_category";
 
     protected $primaryKey = "cc_id";
+
+    protected $fillable = [
+        'category_id',
+        'course_id'
+    ];
 
     public function course()
     {
         return $this->belongsTo(Courses::class);
     }
 
-    public function category()
+    public function detail()
     {
         return $this->belongsTo(Category::class);
     }
